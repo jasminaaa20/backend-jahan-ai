4c535ecc0b81663084baabca2ef085ef04028c7f
Akmal <jasminaaa.20@uom.lk>
2025-02-04 15:40:00 +0530

    Restart project from scratch

--- jahanbackend/user_preferences/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Add your URL patterns here
-    path('account/settings/', views.account_settings, name='account_settings'),
-    path('account/update/', views.update_account_settings, name='update_account_settings'),
-]
-
