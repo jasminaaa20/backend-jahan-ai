fc38b772605000104993dd0ddc1e5cd1c4fbc2f6
Akmal <jasminaaa.20@uom.lk>
2025-02-04 18:26:17 +0530

    Add REST framework and configure authentication URLs

--- user_preferences_backend/settings.py
+++ user_preferences_backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+
+    # Local apps
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
