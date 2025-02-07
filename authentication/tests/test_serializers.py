dd7213b959ed4c84d68f66d193e8189294f629e8
Akmal <jasminaaa.20@uom.lk>
2025-02-07 23:21:31 +0530

    Add tests for ChangePasswordSerializer to validate password change functionality

--- authentication/tests/test_serializers.py
+++ authentication/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from authentication.serializers import RegisterSerializer, LoginSerializer
+from authentication.serializers import RegisterSerializer, LoginSerializer, ChangePasswordSerializer
+from rest_framework.test import APIClient, APIRequestFactory
 
 class RegisterSerializerTests(TestCase):
     def test_valid_data_creates_user(self):
@@ -15,7 +16,6 @@ class RegisterSerializerTests(TestCase):
         user = serializer.save()
         self.assertEqual(user.username, data['username'])
         self.assertEqual(user.email, data['email'])
-        # The password is stored hashed so we use check_password to verify it
         self.assertTrue(user.check_password(data['password']))
     
     def test_password_mismatch(self):
@@ -36,3 +36,33 @@ class LoginSerializerTests(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertIn('username', serializer.errors)
         self.assertIn('password', serializer.errors)
+
+class ChangePasswordSerializerTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='changepassuser', email='changepass@example.com', password='OldPassword123!')
+        self.factory = APIRequestFactory()
+    
+    def test_valid_change_password(self):
+        request = self.factory.post('/', data={}, format='json')
+        request.user = self.user
+        
+        data = {
+            'old_password': 'OldPassword123!',
+            'new_password': 'NewPassword456!',
+            'new_password2': 'NewPassword456!'
+        }
+        serializer = ChangePasswordSerializer(data=data, context={'request': request})
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+    
+    def test_invalid_old_password(self):
+        request = self.factory.post('/', data={}, format='json')
+        request.user = self.user
+        
+        data = {
+            'old_password': 'WrongOldPassword!',
+            'new_password': 'NewPassword789!',
+            'new_password2': 'NewPassword789!'
+        }
+        serializer = ChangePasswordSerializer(data=data, context={'request': request})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('old_password', serializer.errors)
